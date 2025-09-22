571132dcd15c55a47bc63a6b8cd0ae3578345c4e
Danylo Silin <danilsilin76@gmail.com>
2025-09-22 15:58:50 +0300

    Added: request limit

--- src/main/java/com/printhelloworld/linkup_profile_microservice/controller/ProfileController.java
+++ src/main/java/com/printhelloworld/linkup_profile_microservice/controller/ProfileController.java
@@ -12,11 +12,17 @@ import com.printhelloworld.linkup_profile_microservice.entity.UserDto;
 import com.printhelloworld.linkup_profile_microservice.service.ElasticsearchService;
 import com.printhelloworld.linkup_profile_microservice.service.ProfileService;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+
 import java.io.File;
+import java.time.Duration;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,94 +36,116 @@ public class ProfileController {
     private final ProfileService profileService;
     private final ElasticsearchService elasticsearchService;
     private final Logger logger = LoggerFactory.getLogger(ProfileController.class);
+    private final Bucket bucket;
 
     public ProfileController(ProfileService profileService, ElasticsearchService elasticsearchService) {
         this.profileService = profileService;
         this.elasticsearchService = elasticsearchService;
+        Bandwidth limit = Bandwidth.classic(5, Refill.greedy(5, Duration.ofMinutes(1)));
+        this.bucket = Bucket.builder()
+                .addLimit(limit)
+                .build();
     }
 
     @PostMapping("/create")
     public ResponseEntity<User> create(@RequestPart("data") UserDto userDto,
             @RequestPart("avatar") MultipartFile file) {
-        try {
-            // ObjectMapper mapper = new ObjectMapper();
-            // UserDto userDto = mapper.readValue(data, UserDto.class);
+        if (bucket.tryConsume(1)) {
+            try {
+                if (file != null) {
+                    File tempFile = File.createTempFile("avatar-", file.getOriginalFilename());
+                    file.transferTo(tempFile);
 
-            if (file != null) {
-                File tempFile = File.createTempFile("avatar-", file.getOriginalFilename());
-                file.transferTo(tempFile);
+                    userDto.setProfileImage(tempFile);
+                }
 
-                userDto.setProfileImage(tempFile);
+                final User savedUser = profileService.saveProfile(null, userDto);
+
+                return ResponseEntity.ok(savedUser);
+            } catch (Exception e) {
+                logger.error("Failed to create a user", e);
+                e.printStackTrace();
+
+                return ResponseEntity.badRequest().build();
             }
-
-            final User savedUser = profileService.saveProfile(null, userDto);
-
-            return ResponseEntity.ok(savedUser);
-        } catch (Exception e) {
-            logger.error("Failed to create a user", e);
-            e.printStackTrace();
-
-            return ResponseEntity.badRequest().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
         }
     }
 
     @PutMapping("/update/{id}")
     public ResponseEntity<User> update(@PathVariable String id, @RequestPart("data") String data,
             @RequestPart("avatar") MultipartFile file) {
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            UserDto userDto = mapper.readValue(data, UserDto.class);
+        if (bucket.tryConsume(1)) {
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                UserDto userDto = mapper.readValue(data, UserDto.class);
 
-            if (file != null) {
-                File tempFile = File.createTempFile("avatar-", file.getOriginalFilename());
-                file.transferTo(tempFile);
+                if (file != null) {
+                    File tempFile = File.createTempFile("avatar-", file.getOriginalFilename());
+                    file.transferTo(tempFile);
 
-                userDto.setProfileImage(tempFile);
+                    userDto.setProfileImage(tempFile);
+                }
+
+                final User updatedUser = profileService.saveProfile(id, userDto);
+
+                return ResponseEntity.ok(updatedUser);
+            } catch (Exception e) {
+                logger.error("Failed to update user", e);
+
+                return ResponseEntity.badRequest().build();
             }
-
-            final User updatedUser = profileService.saveProfile(id, userDto);
-
-            return ResponseEntity.ok(updatedUser);
-        } catch (Exception e) {
-            logger.error("Failed to update user", e);
-
-            return ResponseEntity.badRequest().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
         }
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<User> delete(@PathVariable String id) {
-        try {
-            profileService.deleteProfile(id);
+        if (bucket.tryConsume(1)) {
+            try {
+                profileService.deleteProfile(id);
 
-            return ResponseEntity.ok().build();
-        } catch (Exception e) {
-            logger.error("Failed to delete user with id: {}", id, e);
+                return ResponseEntity.ok().build();
+            } catch (Exception e) {
+                logger.error("Failed to delete user with id: {}", id, e);
 
-            return ResponseEntity.badRequest().build();
+                return ResponseEntity.badRequest().build();
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
         }
     }
 
     @GetMapping("/find/{query}")
     public ResponseEntity<List<UserDocument>> findUser(@PathVariable String query) {
-        try {
-            List<UserDocument> users = elasticsearchService.searchUsers(query);
-            users.forEach(u -> logger.debug(u.getUsername() + " | " + u.getDisplayName()));
+        if (bucket.tryConsume(1)) {
+            try {
+                List<UserDocument> users = elasticsearchService.searchUsers(query);
+                users.forEach(u -> logger.debug(u.getUsername() + " | " + u.getDisplayName()));
 
-            return ResponseEntity.ok(users);
-        } catch (Exception e) {
-            e.printStackTrace();
-            logger.error("Failed to find user with query: {}", query, e);
+                return ResponseEntity.ok(users);
+            } catch (Exception e) {
+                e.printStackTrace();
+                logger.error("Failed to find user with query: {}", query, e);
 
-            return ResponseEntity.badRequest().build();
+                return ResponseEntity.badRequest().build();
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
         }
     }
 
     @PutMapping("/block-unblock-user/{id}/{blockedId}")
     public ResponseEntity<Void> blockOrUnblockUser(@PathVariable String id, @PathVariable String blockedId) {
-        profileService.blockOrUnblocUser(id, blockedId);
+        if (bucket.tryConsume(1)) {
+            profileService.blockOrUnblocUser(id, blockedId);
 
-        return ResponseEntity.ok().build();
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
     }
 
 }
